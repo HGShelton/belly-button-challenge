a8b37acca072d82b2c1819b67f1b891b93d9f832
HGShelton <hannahgradyshelton@gmail.com>
2024-07-11 15:49:58 -0400

    Build metadata panel, start building charts

--- static/js/app.js
+++ static/js/app.js
@@ -3,35 +3,40 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    // ChatGPT was used to identify '== sample'
+    let filteredMetadata = metadata.filter(obj => obj.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("")
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    // -------------------------------------------------------------- comment
+    Object.entries(filteredMetadata).forEach(([key, value]) => {
+      panel.append("p").text(`${key}: ${value}`);
   });
-}
+})}
 
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples
 
     // Filter the samples for the object with the desired sample number
-
+    let fileteredSamples = samples.filter(obj => obj.id == sample)
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = sampleData.otu_ids;
+    let otu_labels = sampleData.otu_labels;
+    let sample_values = sampleData.sample_values;
 
     // Build a Bubble Chart
 
